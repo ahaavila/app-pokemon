e73561e94194002fac27fa03da9880e71b99eec0
Augusto <ahaavila@gmail.com>
2020-05-02 13:38:05 -0300

    Buscando dados da API
    Adicionado axios. Por meio dele buscamos dados da API e mostramos em tela dinamicamente.

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,62 +1,70 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.png';
 import { Title, Form, Repositories } from './styles';
+import api from '../../services/api';
+
+interface Repository {
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+  height: number;
+  weight: number;
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    const response = await api.get(`pokemon/${newRepo}`);
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Pokemon Explorer" />
       <Title>Explore o mundo pokémon</Title>
 
-      <Form action="">
-        <input type="text" placeholder="Digite o nome do pokémon" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          placeholder="Digite o nome do pokémon"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://manualdosgames.com/wp-content/uploads/2019/05/pikachu-1170x780.jpg"
-            alt="Pikachu"
-          />
-          <div>
-            <strong>Pikachu</strong>
-            <p>It’s in its nature to store electricity.</p>
-          </div>
-          <FiChevronRight size="20" />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://manualdosgames.com/wp-content/uploads/2019/05/pikachu-1170x780.jpg"
-            alt="Pikachu"
-          />
-          <div>
-            <strong>Pikachu</strong>
-            <p>
-              It’s in its nature to store electricity. It feels\nstressed now
-              and then if it’s unable to fully\ndischarge the electricity.
-            </p>
-          </div>
-          <FiChevronRight size="20" />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://manualdosgames.com/wp-content/uploads/2019/05/pikachu-1170x780.jpg"
-            alt="Pikachu"
-          />
-          <div>
-            <strong>Pikachu</strong>
-            <p>
-              It’s in its nature to store electricity. It feels\nstressed now
-              and then if it’s unable to fully\ndischarge the electricity.
-            </p>
-          </div>
-          <FiChevronRight size="20" />
-        </a>
+        {repositories.map((repository) => (
+          <a key={repository.name} href="teste">
+            <img src={repository.sprites.front_default} alt={repository.name} />
+            <div>
+              <strong>{repository.name}</strong>
+              <p>
+                Altura:
+                {repository.height}
+                centímetros
+              </p>
+              <p>
+                Peso:
+                {repository.weight}
+                kg
+              </p>
+            </div>
+            <FiChevronRight size="20" />
+          </a>
+        ))}
       </Repositories>
     </>
   );
